f00a23c3f17462e204e7a7eef3db18b67feea800
Jedidiah Abera <yididi@Jedidiah>
2024-07-01 01:54:28 +0300

    done

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#setting up your client SSH configuration file so that you can connect to a server without typing a password.
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true
+}
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true
+}
