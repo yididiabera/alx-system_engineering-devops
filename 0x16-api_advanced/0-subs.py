2593838f7ea52614c29ba4034d714b3bb09ed75e
Jedidiah Abera <yididi@Jedidiah>
2024-08-13 21:39:11 +0300

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""This script has a function that queries the Reddit API and returns the
-number of all subscribers for a given subreddit"""
+'''
+module for function to query the Reddit API
+'''
+
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers of a subreddit"""
-    if not subreddit or type(subreddit) is not str:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/10.0/API"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False).json()
-    data = response.get('data', {}).get('subscribers', 0)
-    return data
+    results = response.json().get("data")
+    return results.get("subscribers")
